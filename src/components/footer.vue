f042d34f5f1674bbeb62231d60e27356925e1953
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-07 09:09:02 +0300

    created footer page

---
+++ src/components/footer.vue
@@ -0,0 +1,23 @@
+<template>
+    
+        <h1>Task Tracker</h1>
+     
+</template>
+
+
+
+<script>
+export default {
+    name:'head_der',
+}
+</script>
+
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
